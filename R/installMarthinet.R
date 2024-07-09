37ce625913f3cfe3d45b4a4b8fef3859c9d977c3
Hugo Marthinet <h.marthinet@gmail.com>
2024-07-09 15:05:00 +0200

    Update and rename installMarthinet to installMarthinet.R

--- R/installMarthinet.R
+++ R/installMarthinet.R
@@ -1,8 +1,8 @@
 installMarthinet <- function(){
-  remotes::install_github("HugoMrth/manageR")
   remotes::install_github("HugoMrth/epiplus")
-  remotes::install_github("HugoMrth/dataDesc")
   remotes::install_github("HugoMrth/lazyMe")
+  remotes::install_github("HugoMrth/manageR")
   remotes::install_github("HugoMrth/surveyscores")
   remotes::install_github("HugoMrth/sampleSize")
+  remotes::install_github("HugoMrth/dataDesc")
 }
