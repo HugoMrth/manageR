fc8e5a5792f211692a93f33b21430e7da6dfa78f
Hugo Marthinet <h.marthinet@gmail.com>
2024-11-20 08:59:24 +0100

    Add files via upload

--- inst/logo.R
+++ inst/logo.R
@@ -10,16 +10,16 @@ sticker(
   subplot = "inst/image.png",
   filename = "inst/logo.png",  # Output file name
   #img = "C:/Users/MARTHINETEX/Documents/02 - Code/logo.png",     # Path to your background image
-  package = "dataDesc",                 # Package name
-  p_size = 26,
-  p_y = 1.5,
-  p_x = 1,# Text size
-  s_x = 1,                            # X position of the image
-  s_y = 0.88,                         # Y position of the image
-  s_width = 0.65,                      # Width of the image
+  package = "anageR",                 # Package name
+  p_size = 24,
+  p_y = 1,
+  p_x = 1.33,# Text size
+  s_x = 0.5,                            # X position of the image
+  s_y = 1,                         # Y position of the image
+  s_width = 0.35,                      # Width of the image
   h_fill = "#FFFFFF",                 # Background color
-  h_color = "orange2",                # Hex border color
-  p_color = "orange2",                # Package name color
+  h_color = "#201A1C",                # Hex border color
+  p_color = "#201A1C",                # Package name color
   layout = "straight",                # Text layout
   text_y = 1.2                       # Y position of the text
-) 
+)
